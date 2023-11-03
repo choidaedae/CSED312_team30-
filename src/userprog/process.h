1cfb7b410da450c27d6c25b689172846ee8e6e38
howru0321 <howru0321@naver.com>
2023-11-03 22:41:24 +0900

    good

---
+++ src/userprog/process.h
@@ -0,0 +1,23 @@
+#ifndef USERPROG_PROCESS_H
+#define USERPROG_PROCESS_H
+
+#include "threads/thread.h"
+#include "userprog/syscall.h"
+#include "vm/page.h"
+
+tid_t process_execute (const char *file_name);
+int process_wait (tid_t);
+void process_exit (void);
+void process_activate (void);
+
+void construct_esp(char *file_name, void **esp);
+int fn_to_argument(char **argv, char *file_name);
+void argument_stack(char **argv, int argc, void **esp);
+struct thread *get_child_process (pid_t pid);
+void remove_child_process(struct thread *cp);
+
+int process_add_file (struct file *f);
+struct file *process_get_file(int fd);
+void process_close_file(int fd);
+
+#endif /* userprog/process.h */
---
+++ src/userprog/syscall.h
@@ -0,0 +1,27 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include <stdbool.h>
+typedef int pid_t;
+typedef int mapid_t;
+
+
+void get_argument(void *esp, int *arg, int count);
+
+void syscall_init (void);
+void halt (void);
+void exit (int status);
+pid_t exec (const char *cmd_lime);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
+
+#endif /* userprog/syscall.h */
